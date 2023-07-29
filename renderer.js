8503fb38f9e6925f85a7bc4e0cde64b43927acbe
Anirban Dey <116617392+anirbandey1@users.noreply.github.com>
2023-07-29 12:17:41 +0530

    Functioning file convertor

--- renderer.js
+++ renderer.js
@@ -2,6 +2,23 @@ const ipc = require('electron').ipcRenderer
 // ipcRenderer is used to send events to the main process 
 const button = document.querySelector("#upload")
 
+var format = 'mp3'
+var dir = './media'
+const fs = require('fs')
+const $ = require('jquery')
+const process = require("child_process")
+// node runs inside a single process but you can child processes as well
+
+
+if(!fs.existsSync(dir)){
+    fs.mkdirSync(dir)
+}
+
+$("#format").change(()=>{
+    format = $("#format option:selected").text()
+    console.log("New format : "+format)
+})
+
 button.addEventListener("click", function(event){
     ipc.send('open-file-dialog-for-file')
 })
@@ -9,4 +26,19 @@ button.addEventListener("click", function(event){
 ipc.on('selected-file',(event,paths)=>{
     console.log(event)
     console.log(paths)
+    
+    var filename = paths.replace(/^.*[\\\/]/, '')
+    // this will convert full path to filename
+    filename = filename.replace(/\.[^/.]+$/, "")
+    // remove extension
+
+    // execute ffmpeg command to perform file conversion
+    process.exec(`ffmpeg -i ${paths} media/${filename}_converted.${format}`, (error,stdout,stderr)=>{
+       console.log(stdout)
+
+        if(error != null){
+            console.log(error)
+        }
+
+    }) 
 })
